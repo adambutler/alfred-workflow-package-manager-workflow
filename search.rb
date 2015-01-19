7c288b8e48d7bc7f2e3f2317ecedb315796fe88c
Adam Butler <adam@lab.io>
2015-01-19 14:50:32 +0000

    Start search support

---
+++ search.rb
@@ -0,0 +1,17 @@
+puts """
+<?xml version='1.0'?>
+<items>
+    <item uid='alpha' autocomplete='alpha'>
+        <title>Alpha</title>
+        <icon>workflow.png</icon>
+    </item>
+    <item uid='bravo' autocomplete='bravo'>
+        <title>Bravo</title>
+        <icon>workflow.png</icon>
+    </item>
+    <item uid='charlie' autocomplete='charlie'>
+        <title>Charlie</title>
+        <icon>workflow.png</icon>
+    </item>
+</items>
+"""
