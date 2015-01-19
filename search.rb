985a79277fb6cef7bf4a94b3a6cb632d401338ca
Adam Butler <adam@lab.io>
2015-01-19 21:57:10 +0000

    Create the XML list dynamically

--- search.rb
+++ search.rb
@@ -1,17 +1,45 @@
-puts """
-<?xml version='1.0'?>
-<items>
-    <item uid='alpha' autocomplete='alpha'>
-        <title>Alpha</title>
+require 'rubygems'
+require 'net/http'
+require 'json'
+
+def generate_xml(workflows)
+  xml = []
+  xml << "<?xml version='1.0'?><items>"
+
+  workflows.each do |workflow|
+    xml << """
+      <item uid='#{workflow["title"]}' autocomplete='#{workflow["title"]}'>
+        <title>#{workflow["title"]}</title>
         <icon>workflow.png</icon>
-    </item>
-    <item uid='bravo' autocomplete='bravo'>
-        <title>Bravo</title>
-        <icon>workflow.png</icon>
-    </item>
-    <item uid='charlie' autocomplete='charlie'>
-        <title>Charlie</title>
-        <icon>workflow.png</icon>
-    </item>
-</items>
-"""
+      </item>"""
+  end
+
+  xml << "</items>"
+  puts xml.join
+end
+
+def search(id)
+  begin
+    uri = URI("http://alfred-workflow-package-manager.dev/workflows.json")
+
+    # Create client
+    http = Net::HTTP.new(uri.host, uri.port)
+
+    # Create Request
+    request =  Net::HTTP::Get.new(uri)
+
+    # Fetch Request
+    response = http.request(request)
+
+    # Parse the response and get the git_repository_url
+    workflows = JSON.parse(response.body)
+
+    # Generate the XML list of workflows
+    generate_xml(workflows)
+
+  rescue Exception => e
+    puts "HTTP Request failed (#{e.message})"
+  end
+end
+
+search("html")
