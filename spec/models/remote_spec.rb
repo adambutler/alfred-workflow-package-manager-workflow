768940ba32897629b93888b94250c36f162220be
Adam Butler <adam@lab.io>
2015-03-08 18:41:09 +0000

    + Remote specs

--- spec/models/remote_spec.rb
+++ spec/models/remote_spec.rb
@@ -1,2 +1,19 @@
 describe Workflow::Remote do
+  context "index" do
+    it "returns an array" do
+      expect(Workflow::Remote.index).to be_kind_of(Array)
+    end
+  end
+
+  context "get" do
+    it "returns an array" do
+      expect(Workflow::Remote.get(1)).to be_kind_of(Hash)
+    end
+  end
+
+  context "search" do
+    it "returns an array" do
+      expect(Workflow::Remote.search("testing")).to be_kind_of(Array)
+    end
+  end
 end
